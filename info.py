84988a17df77a0649455004b53dd2159c2b53e2d
Babukuruvila <122657353+Babukuruvila@users.noreply.github.com>
2024-03-30 11:08:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -44,7 +44,7 @@ NO_RESULTS_MSG = is_enabled((environ.get("NO_RESULTS_MSG", 'False')), False)
 
 # MongoDB information
 SECONDDB_URI = environ.get('SECONDDB_URI', "mongodb+srv://inoxbotdb:inoxbotdb@cluster0.iarv6iy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://inox:inox@cluster0.cq3jwmi.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://inox:inox@cluster0.cq3jwmi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Rajappan")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
