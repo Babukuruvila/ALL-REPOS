f6b54ec0f1994f3bec2317c6f70bec488eab222a
SᴛᴇʟʟᴀʀLᴀʙs <158211667+Daneil-k@users.noreply.github.com>
2024-07-24 17:11:00 +0530

    Update join_req.py

--- plugins/join_req.py
+++ plugins/join_req.py
@@ -7,6 +7,7 @@ from pyrogram import Client, filters, enums
 from pyrogram.types import ChatJoinRequest
 from database.join_reqs import JoinReqs
 from info import ADMINS, REQ_CHANNEL
+from pyrogram.types import Message
 import os
 import sys
 
