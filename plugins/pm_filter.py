76cecbc947a0a6730d9a9bd452a07b141fc260e0
Babukuruvila <122657353+Babukuruvila@users.noreply.github.com>
2024-07-24 22:31:07 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -6,7 +6,9 @@ import math
 import random
 import pyrogram
 lock = asyncio.Lock()
-
+from database.join_reqs import JoinReqs
+import os 
+import sys
 from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 from Script import script
 from database.connections_mdb import active_connection, all_connections, delete_connection, if_active, make_active, \
